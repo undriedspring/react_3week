c17b89983732c672dd45ae7c1ee402decb3a28d7
undriedspring <hansam5615@gmail.com>
2021-12-01 00:23:29 +0900

    3rd_commit

---
+++ src/redux/modules/post.js
@@ -0,0 +1,82 @@
+import { createAction, handleActions } from 'redux-actions'
+import { produce } from 'immer'
+import { firestore } from '../../shared/firebase'
+
+const SET_POST = 'SET_POST'
+const ADD_POST = 'ADD_POST'
+
+const setPost = createAction(SET_POST, (post_list) => ({ post_list }))
+const addPost = createAction(ADD_POST, (post) => ({ post }))
+
+const initialState = {
+  list: [],
+}
+
+const initialPost = {
+  id: 0,
+  user_info: {
+    user_name: '213am',
+    user_profile: 'http://newsimg.hankookilbo.com/2019/04/29/201904291390027161_3.jpg',
+  },
+  image_url: 'http://newsimg.hankookilbo.com/2019/04/29/201904291390027161_3.jpg',
+  contents: '고양이네요!',
+  comment_cnt: 10,
+  insert_dt: '2021-11-30 10:00:00',
+}
+
+const addPostFB = () => {
+  return function (dispatch, getState, { history }) {}
+}
+
+const getPostFB = () => {
+  return function (dispatch, getState, { history }) {
+    const postDB = firestore.collection('post')
+
+    postDB.get().then((docs) => {
+      let post_list = []
+      docs.forEach((doc) => {
+        let _post = {
+          id: doc.id,
+          ...doc.data(),
+        }
+
+        let post = {
+          id: doc.id,
+          user_info: {
+            user_name: _post.user_name,
+            user_profile: _post.user_profile,
+            user_id: _post.user_id,
+          },
+          image_url: _post.image_url,
+          contents: _post.contents,
+          comment_cnt: _post.comment_cnt,
+          insert_dt: _post.insert_dt,
+        }
+
+        post_list.push(post)
+      })
+
+      dispatch(setPost(post_list))
+    })
+  }
+}
+
+export default handleActions(
+  {
+    [SET_POST]: (state, action) =>
+      produce(state, (draft) => {
+        draft.list = action.payload.post_list
+      }),
+
+    [ADD_POST]: (state, action) => produce(state, (draft) => {}),
+  },
+  initialState
+)
+
+const actionCreators = {
+  setPost,
+  addPost,
+  getPostFB,
+}
+
+export { actionCreators }
