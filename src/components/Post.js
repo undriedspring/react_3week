66873db1ab576826602b07f4b66073a421aec7c1
undriedspring <hansam5615@gmail.com>
2021-11-30 13:13:38 +0900

    first commit

---
+++ src/components/Post.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Grid, Image, Text } from '../elements'
+
+const Post = (props) => {
+  return (
+    <React.Fragment>
+      <Grid>
+        <Grid is_flex>
+          <Image shape="circle" src={props.src} />
+          <Text bold>{props.user_info.user_name}</Text>
+          <Text>{props.insert_dt}</Text>
+        </Grid>
+        <Grid padding="16px">
+          <Text>{props.contents}</Text>
+        </Grid>
+        <Grid>
+          <Image shape="rectangle" src={props.src} />
+        </Grid>
+        <Grid padding="16px">
+          <Text bold>댓글 {props.comment_cnt}개</Text>
+        </Grid>
+        <Grid></Grid>
+        <div>user profile / user name / insert_dt</div>
+        <div>contents</div>
+        <div>image</div>
+        <div>comment cnt</div>
+      </Grid>
+    </React.Fragment>
+  )
+}
+
+Post.defaultProps = {
+  user_info: {
+    user_name: '213am',
+    user_profile: 'http://newsimg.hankookilbo.com/2019/04/29/201904291390027161_3.jpg',
+  },
+  image_url: 'http://newsimg.hankookilbo.com/2019/04/29/201904291390027161_3.jpg',
+  contents: '고양이네요!',
+  comment_cnt: 10,
+  insert_dt: '2021-11-30 10:00:00',
+}
+
+export default Post
---
+++ src/elements/Image.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Image = (props) => {
+  const { shape, src, size } = props
+  const styles = {
+    src: src,
+    size: size,
+  }
+  if (shape === 'circle') {
+    return <ImageCircle {...styles}></ImageCircle>
+  }
+  if (shape === 'rectangle') {
+    return (
+      <AspectOutter>
+        <AspectInner {...styles}></AspectInner>
+      </AspectOutter>
+    )
+  }
+  return <React.Fragment></React.Fragment>
+}
+
+Image.defaultProps = {
+  shpae: 'circle',
+  src: 'http://newsimg.hankookilbo.com/2019/04/29/201904291390027161_3.jpg',
+  size: 36,
+}
+
+const ImageCircle = styled.div`
+  --size: ${(props) => props.size}px;
+  width: var(--size);
+  height: var(--size);
+  border-radius: var(--size);
+
+  background-image: url('${(props) => props.src}');
+  background-size: cover;
+  margin: 4px;
+`
+
+const AspectOutter = styled.div`
+  width: 100%;
+  min-width: 250px;
+`
+
+const AspectInner = styled.div`
+  position: relative;
+  padding-top: 75%;
+  overflow: hidden;
+  background-image: url('${(props) => props.src}');
+  background-size: cover;
+`
+
+export default Image
